732274814b270430ccdadb8212a7d29fb05110a7
chenzhian <1240780563@qq.com>
2020-07-22 17:36:06 +0800

    加入显示天气信息的功能。

--- app/src/main/java/com/coolweather/android/util/Utility.java
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.coolweather.android.db.City;
 import com.coolweather.android.db.County;
 import com.coolweather.android.db.Province;
+import com.coolweather.android.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -39,18 +41,18 @@ public class Utility {
     public static boolean handleCityResponse(String response, int provinceId){
         if (!TextUtils.isEmpty(response)){
                 try {
-                JSONArray allCities = new JSONArray(response);
-                for (int i = 0; i < allCities.length(); i++){
-                    JSONObject cityObject = allCities.getJSONObject(i);
-                    City city = new City();
-                    city.setCityName(cityObject.getString("name"));
-                    city.setCityCode(cityObject.getInt("id"));
-                    city.setProvinceId(provinceId);
-                    city.save();
-                }
-                return true;
-            }catch (JSONException e){
-                e.printStackTrace();
+                    JSONArray allCities = new JSONArray(response);
+                    for (int i = 0; i < allCities.length(); i++){
+                        JSONObject cityObject = allCities.getJSONObject(i);
+                        City city = new City();
+                        city.setCityName(cityObject.getString("name"));
+                        city.setCityCode(cityObject.getInt("id"));
+                        city.setProvinceId(provinceId);
+                        city.save();
+                    }
+                    return true;
+             }catch (JSONException e){
+                    e.printStackTrace();
             }
         }
         return false;
@@ -77,5 +79,20 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
